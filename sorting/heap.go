0849a4a96ef031a0ea5ce33b5d9f1015d5f42311
Dubjay18 <jejeniyi7@gmail.com>
2024-11-20 14:23:35 +0100

    feat: heap sort

---
+++ sorting/heap.go
@@ -0,0 +1,42 @@
+package sorting
+
+func HeapSort(arr []int) []int {
+	// Build heap (rearrange array)
+	for i := len(arr)/2 - 1; i >= 0; i-- {
+		heapify(arr, len(arr), i)
+	}
+
+	// One by one extract an element from heap
+	for i := len(arr) - 1; i > 0; i-- {
+		// Move current root to end
+		arr[0], arr[i] = arr[i], arr[0]
+
+		// call max heapify on the reduced heap
+		heapify(arr, i, 0)
+	}
+	return arr
+}
+
+func heapify(arr []int, n, i int) {
+	largest := i // Initialize largest as root
+	l := 2*i + 1 // left = 2*i + 1
+	r := 2*i + 2 // right = 2*i + 2
+
+	// If left child is larger than root
+	if l < n && arr[l] > arr[largest] {
+		largest = l
+	}
+
+	// If right child is larger than largest so far
+	if r < n && arr[r] > arr[largest] {
+		largest = r
+	}
+
+	// If largest is not root
+	if largest != i {
+		arr[i], arr[largest] = arr[largest], arr[i]
+
+		// Recursively heapify the affected sub-tree
+		heapify(arr, n, largest)
+	}
+}
